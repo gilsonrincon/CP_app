3310e7a433baa5e8cca1dcb921c1ec2b3ba5b504
Gilson Rincón <gilson.rincon@gmail.com>
2018-05-01 21:47:13 -0500

    Page added for individual posts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PostPage } from '../post/post';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	items: any[];
+	texto: string;
 
   constructor(public navCtrl: NavController) {
+  	this.items = [];
+  	this.texto = "prueba";
 
+  	for(let i = 0; i<= 10; i++){
+  		this.items.push({
+  			text: "Item N"+i,
+  			id: i
+  		});
+  	}
+
+  }
+
+  ItemSelected(item){
+  	this.navCtrl.push(PostPage, { item: item });
   }
 
 }
