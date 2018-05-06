98ded18cdc269043dd932f262542ef366c0ec894
Gilson Rincón <gilson.rincon@gmail.com>
2018-05-06 11:48:37 -0500

    Contact page and some UI improvements

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PostPage } from '../post/post';
 import { PostsProvider } from '../../providers/posts/posts';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -12,6 +13,10 @@ export class HomePage {
   postsList: any[];
 
   constructor(public navCtrl: NavController, private postProvider: PostsProvider) {
+
+  }
+
+  ionViewDidLoad(){
     this.postProvider.getPosts().subscribe(res => {
       this.postsList = [];
       for(let key in res){
@@ -28,9 +33,15 @@ export class HomePage {
   }
 
   postImage(id, index){
-    //console.log(index);
     this.postProvider.getPostMedia(id).subscribe(data => {
-      this.postsList[index].media_url = data.source_url;
+      let data2 = [];
+      data2.push(data);
+      for(let k in data2){
+          this.postsList[index].media_url = data2[k].guid.rendered;
+      }
+
+
+
     });
   }
 
--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PostsProvider } from '../../providers/posts/posts';
 
@@ -20,18 +21,29 @@ export class PostPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private postProvider: PostsProvider) {
     this.post = {
-      id: this.navParams.get('id'),
+      id: this.navParams.data.id,
       content: "",
-      media_url: "",
       title: "",
-      image: ""
+      media_url: ""
     };
 
     this.postProvider.getPost(this.post.id).subscribe(res => {
-      console.log(res);
-      this.post.content = res.content.rendered;
-      this.post.title = res.title.rendered;
+      let res_arr = [];
+      res_arr.push(res);
+      for(let k in res_arr){
+        this.post.content = res_arr[k].content.rendered;
+        this.post.title = res_arr[k].title.rendered;
+        this.postImage(res_arr[k].featured_media);
+      }
+    });
+  }
 
+  postImage(id){
+    this.postProvider.getPostMedia(id).subscribe(data => {
+      let data_arr = [];
+      data_arr.push(data);
+      for(let k in data_arr)
+        this.post.media_url = data_arr[k].source_url;
     });
   }
 
